9931f94d44d9f1567dbaf29fe59c9f645e9c7ad0
RestoreMonarchy <35507241+RestoreMonarchy@users.noreply.github.com>
2020-02-15 19:08:18 +0100

    fix uconomy queries case error

--- Marketplace/Server/Database/UconomyExtensions.cs
+++ Marketplace/Server/Database/UconomyExtensions.cs
@@ -8,7 +8,7 @@ namespace Marketplace.Server.Database
     {
         public static void UconomyPay(this MySqlConnection conn, string playerId, decimal amount)
         {
-            string sql = "UPDATE Uconomy SET balance = balance + @amount, lastUpdated = @date WHERE steamId = @playerId;";
+            string sql = "UPDATE uconomy SET balance = balance + @amount, lastUpdated = @date WHERE steamId = @playerId;";
             using (conn)
             {
                 conn.Execute(sql, new { amount, date = DateTime.Now, playerId });
@@ -17,7 +17,7 @@ namespace Marketplace.Server.Database
 
         public static decimal UconomyGetBalance(this MySqlConnection conn, string playerId)
         {
-            string sql = "SELECT balance FROM Uconomy WHERE steamId = @playerId;";
+            string sql = "SELECT balance FROM uconomy WHERE steamId = @playerId;";
             using (conn)
             {
                 return conn.ExecuteScalar<decimal>(sql, new { playerId });
